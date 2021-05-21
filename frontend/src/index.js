b47807da8f2b090e38fb067dc6ccc44f14d03023
Davis <handlerdavis@gmail.com>
2021-05-21 14:53:00 -0600

    finished front end auth

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,38 +1,38 @@
-// import components and packages
+// frontend/src/index.js
 import React from "react";
-import "./index.css";
 import ReactDOM from "react-dom";
+import "./index.css";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { ModalProvider } from "./context/Model";
+
 import configureStore from "./store";
 import { restoreCSRF, csrfFetch } from "./store/csrf";
 import * as sessionActions from "./store/session";
 
-// import store
 const store = configureStore();
-if (process.env.NODE_ENV !== "production") {
-  window.store = store;
-}
+
 if (process.env.NODE_ENV !== "production") {
   restoreCSRF();
+
   window.csrfFetch = csrfFetch;
   window.store = store;
   window.sessionActions = sessionActions;
 }
 
-// root application
 function Root() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <ModalProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ModalProvider>
     </Provider>
   );
 }
 
-// render root
 ReactDOM.render(
   <React.StrictMode>
     <Root />
